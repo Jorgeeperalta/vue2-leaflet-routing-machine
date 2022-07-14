d962efff14cd5a9bdf0ad3165c88b48be775d1b6
Jorgeeperalta <edudela36@gmail.com>
2022-07-14 13:17:16 -0300

    adactando con localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,66 @@
 <template>
-  <div id="app">
+<v-app>
+  <div  id="app">
     <l-map :zoom="zoom" :center="center">
       <l-tile-layer
         :url="osmUrl"
         :attribution="attribution"/>
-      <l-routing-machine :waypoints="waypoints"/>
+      <l-routing-machine :language="language" :waypoints="waypoints"/>
     </l-map>
+     <br><br><br>
+   
   </div>
+   <input v-model="latitud"/>
+     <input v-model="longitud"/>
+       <button @click="guarda">Guarda</button>
+     <button @click="abre">Abre</button>
+    
+    <Mapa v-if="bandera"> </Mapa>
+    </v-app>
 </template>
 
 <script>
 import { LMap, LTileLayer } from 'vue2-leaflet'
 import LRoutingMachine from './components/LRoutingMachine'
-
+import Mapa from './components/MapaRuta'
 const attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-const osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+const osmUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
 export default {
-  components: { LMap, LTileLayer, LRoutingMachine },
+  components: { LMap, LTileLayer, LRoutingMachine, Mapa },
   data () {
     return {
+      latitud:0,
+      longitud:0,
+      bandera:false,
       zoom: 6,
-      center: { lat: 38.7436056, lng: -2.2304153 },
+      center: { lat:-36.72, lng: -57.2304153 },
       osmUrl,
       attribution,
+      language: 'es', 
       waypoints: [
-        { lat: 38.7436056, lng: -9.2304153 },
-        { lat: 38.7436056, lng: -0.131281 }
+        { lat: -36.736056, lng: -56.7404153 },
+        { lat: -36.736056, lng: -56.731281 }
       ]
     }
+  },
+  mounted() {
+      
+  },
+  methods:{
+      guarda(){
+       sessionStorage.removeItem('lat')
+       sessionStorage.removeItem('lng')
+         alert(sessionStorage.lat)
+              sessionStorage.lat=this.latitud
+              sessionStorage.lng=-this.longitud
+               alert(sessionStorage.lat)
+      },
+      abre(){
+            this.bandera= true
+      }
+     
+     
   }
 }
 
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    coordenadasUsuario:[
+        { lat: -36.5, lng: -56.8 },
+        { lat: -36.7, lng: -56.7 },
+      ]
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
